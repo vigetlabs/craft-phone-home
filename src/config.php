eb8d030b7660e3085c232eb77ab62ddcec4d3f97
Joshua Pease <joshua.pease@viget.com>
2024-03-27 16:40:19 -0700

    [n/a] Add example config file

---
+++ src/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Phone Home config.php
+ *
+ * This file exists only as a template for Phone Home settings.
+ *
+ * Copy this file to 'craft/config' as 'phone-home.php'
+ * and make your changes there to override default settings.
+ */
+
+return [
+    // Whether phone home is enabled. When set to null (default), the plugin
+    // will phone home when Craft is not in devMode
+    // 'enabled' => null,
+
+    // Configure the Notion API. If these env vars are not present, the plugin will not send to Endpoint
+    // 'notion' => [
+    //     'secret' => \craft\helpers\App::env('PHONE_HOME_NOTION_SECRET'),
+    //     'databaseId' => \craft\helpers\App::env('PHONE_HOME_NOTION_DATABASE'),
+    // ],
+];
